a0cc9603d92aca58846c3fe2fc9f0e9218a79c66
Aditya <adityamedipelly3@gmail.com>
2024-12-27 22:27:18 +0530

    first commit

---
+++ src/components/Auth/Login.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react"
+
+
+function Login() {
+ 
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+
+    const submitHandler = (e) => {
+        e.preventDefault()
+      console.log("email")
+      console.log("password")
+
+      setEmail("")
+      setPassword("")
+    }
+
+
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <div className="border-2 rounded-xl border-emerald-600 p-20">
+       <form 
+       onSubmit={(e)=>{
+        submitHandler(e)
+       }}
+       className="flex flex-col items-center justify-center">
+
+        <input
+        value={email}
+        onChange={(e)=>{
+            setEmail(e.target.value)  
+        }}
+         required className="border-2  outline-none bg-transparent border-emerald-600 py-3 px-5 text-xl rounded-full placeholder:text-gray-400" type = "email" placeholder=" enter your email"/>
+        <input 
+        value={password}
+        onChange={(e)=>{
+            setPassword(e.target.value)
+        }}
+        required className= "border-2  outline-none bg-transparent border-emerald-600 py-3 px-5 text-xl rounded-full placeholder:text-gray-400 mt-3"  type = "password" placeholder=" enter your password"/>
+        <button className= " text-white border-none outline-none  bg-emerald-600 py-3 px-5 text-xl w-full rounded-full placeholder:text-white mt-10">Log in</button>
+       </form>
+      </div>
+    </div>
+  )
+}
+
+export default Login
